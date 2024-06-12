00db920b82032f6ccb633773f5831f85da08385e
Vlad <lebedevvladislav439@gmail.com>
2024-06-12 13:52:16 +0300

    исправлены ошибки #1

--- lr3/Program.cs
+++ lr3/Program.cs
@@ -33,7 +33,7 @@ namespace lr3
                     Console.Write("Радиус R = ");
                     temp = Console.ReadLine();
                     Rad = Convert.ToDouble(temp);
-                    Console.WriteLine("Площадь круга = " + 4 * Atan(1.0) * Pow(Rad, 2.0));
+                    Console.WriteLine("Площадь круга = " + 4 * Math.Atan(1) * Math.Pow(Rad, 2));
                     break;
 
                 default: Console.WriteLine("Выбор неверен "); break;
